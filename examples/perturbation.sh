584e2c4a6fd9258dd1a657c8de8efe1d3df9a6ad
MeisongShi <shims15301985235@163.com>
2026-08-19 16:02:26 +0800

    Update perturbation.sh

--- examples/perturbation.sh
+++ examples/perturbation.sh
@@ -6,9 +6,6 @@ epic-perturbation \
   --gene-name GENE_NAME \
   --cellline CELL_LINE \
   --model-name MODEL_NAME \
-  --cre-length 1000 \
-  --notrain-data-path tests/fixtures/synthetic_input.json \
-  --save-path results/perturbation.json \
   --H3K4me1-factor 1.0 \
   --H3K4me2-factor 1.0 \
   --H3K4me3-factor 1.0 \
@@ -16,8 +13,5 @@ epic-perturbation \
   --H3K9ac-factor 1.0 \
   --H3K27me3-factor 1.0 \
   --WGBS-factor 1.0 \
-  --mean-value 0.0 \
-  --limit-up-factor 1.0 \
-  --limit-down-factor 1.0 \
   --dry-run
 
